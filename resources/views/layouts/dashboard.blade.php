aa95a008e1e4bf8181872f66cf32cca84565b2b5
Heru Rizky Fajar <herurizkyfajar@gmail.com>
2026-07-13 09:54:19 +0700

    Fix car icon styling to match sidebar green color and spacing

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -142,7 +142,7 @@
                         </li>
                         <li class="{{ request()->routeIs('admin.cars.*') ? 'active' : '' }}">
                             <a href="{{ route('admin.cars.index') }}">
-                                <i class="icon-uniE914"></i>
+                                <svg width="18" height="18" viewBox="0 0 24 24" fill="#4DA528" xmlns="http://www.w3.org/2000/svg" style="margin-right:14px;vertical-align:middle;"><path d="M18.92 6.01C18.72 5.42 18.16 5 17.5 5h-11c-.66 0-1.21.42-1.42 1.01L3 12v8c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-1h12v1c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-8l-2.08-5.99zM6.5 16c-.83 0-1.5-.67-1.5-1.5S5.67 13 6.5 13s1.5.67 1.5 1.5S7.33 16 6.5 16zm11 0c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zM5 11l1.5-4.5h11L19 11H5z"/></svg>
                                 <span>Cars</span>
                             </a>
                         </li>
